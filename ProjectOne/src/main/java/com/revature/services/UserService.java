bac3d240e69dc5d8dbd8b7fcf7391c856c0788e7
Jasmine McCall <jasmine@Jasmines-MacBook-Pro.local>
2021-08-01 17:55:24 -0500

    initial db setup

--- ProjectOne/src/main/java/com/revature/services/UserService.java
+++ ProjectOne/src/main/java/com/revature/services/UserService.java
@@ -1,4 +1,4 @@
-package com.revature.main;
+package com.revature.services;
 
 import com.revature.beans.User;
 
--- ProjectOne/src/main/java/com/revature/services/UserServiceImpl.java
+++ ProjectOne/src/main/java/com/revature/services/UserServiceImpl.java
@@ -3,12 +3,16 @@ package com.revature.services;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.revature.beans.User;
 import com.revature.data.UserDAOImpl;
-import com.revature.main.UserService;
 
 public class UserServiceImpl implements UserService {
 	UserDAOImpl ud = new UserDAOImpl();
+	private Logger log = LogManager.getLogger(UserServiceImpl.class);
 
 	
 	public User login(String name, String password) {
